21028b91ecc001f77c5ba4fded1a1d538bcff85f
Sagar Raut <sagarraut2059@gmail.com>
2024-07-23 17:43:44 +0545

    apiData

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,35 @@
-const HomePage = () => {
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-  // asynchronous function --3states--pending--fulfilled--rejected
-  setTimeout(() => {
-    console.log("Hello 1");
-  }, 2000);
-  console.log("Hello 2");
+const HomePage = () => {
+  const [data, setData] = useState(null);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        "https://www.themealdb.com/api/json/v1/1/categories.php"
+      );
+      setData(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
-      <h1>Hello</h1>
+      {data?.categories.map((category, index) => (
+        <div key={index}>
+          <h3>{category.strCategory}</h3>
+          <p>{category.strCategoryDescription}</p>
+          <img src={category.strCategoryThumb} alt={category.strCategory} />
+        </div>
+      ))}
     </div>
   );
 };
+
 export default HomePage;
